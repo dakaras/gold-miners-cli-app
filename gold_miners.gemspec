97d56cdb18396ce69659228339f1814e0cbdd598
Daniel Karagiannis <dakaragiannis@gmail.com>
2017-11-09 17:57:23 +0000

    Done.

--- gold_miners.gemspec
+++ gold_miners.gemspec
@@ -33,3 +33,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.16"
   spec.add_development_dependency "rake", "~> 10.0"
 end
+# The gemspec defines what’s in the gem, who made it, and the version of the gem.
+# It’s also your interface to RubyGems.org.
--- lib/gold_miners.rb
+++ lib/gold_miners.rb
@@ -1,3 +1,7 @@
-# this is the environment file 
+# this is the environment file
+# The convention is to have one Ruby file with the same name as your gem,
+# since that gets loaded when require 'gold_miners' is run.
+# the gem’s root file is in charge of loading code for the gem.
+# The other files for a gem are usually placed in a directory of the same name of the gem inside of lib. 
 require_relative "./gold_miners/version"
 require_relative './gold_miners/cli'
