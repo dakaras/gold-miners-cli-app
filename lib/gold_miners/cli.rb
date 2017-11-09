603deac3aaa191c35ebb184e9c0bfd0defd1996c
Daniel Karagiannis <dakaragiannis@gmail.com>
2017-11-09 18:58:14 +0000

    Done.

--- lib/gold_miners/cli.rb
+++ lib/gold_miners/cli.rb
@@ -7,26 +7,29 @@ class GoldMiners::CLI
   end
 
   def list_quotes
-    puts "QUOTES FROM TODAY'S SESSION ARE AVAILABLE FOR THE FOLLOWING:"
+    puts "QUOTES FROM TODAY'S SESSION ARE AVAILABLE FOR THE FOLLOWING GOLD MINING STOCKS:"
     puts "1. Ticker: NEM - Name: Newmont Mining"
     puts "2. Ticker: GG - Name: Goldcorp"
   end
 
   def menu
-    puts "Enter a number for the stock you would like additional info on."
-    puts "Type exit when finished."
-    input = gets.to_i.strip
-
+    input = nil
     while input != "exit"
+      puts "Enter a number for the stock you would like additional info on."
+      puts "Enter list to display all stocks."
+      puts "Enter exit when finished."
+      input = gets.strip.downcase’
       case input
-      when input-1 == "1"
+      when input == "1"
         puts "1. Ticker: NEM - Name: Newmont Mining - Price: 36.29 - Change: 1.68% - Volume: 2,404,011"
       when "2"
         puts "2. Ticker: GG - Name: Goldcorp - Price: 13.10 - Change: -0.53% - Volume: 1,955,126"
+      when "list"
+        list_quotes
       else
-        puts "Invalid entry. Please re-enter desired selection. Enter list to show all quotes or exit to finish."
+        puts "Invalid entry. Please re-enter desired selection. Enter list to display all stocks or exit to finish."
       end
     end
   end
-  
+
 end
