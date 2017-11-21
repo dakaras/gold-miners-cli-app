4bae461fbdde0384fcf4651ac43119edef019ac3
Daniel Karagiannis <dakaragiannis@gmail.com>
2017-11-21 01:55:46 +0000

    Done.

--- lib/gold_miners/cli.rb
+++ lib/gold_miners/cli.rb
@@ -12,9 +12,9 @@ class GoldMiners::CLI
     puts "QUOTES FROM TODAY'S SESSION ARE AVAILABLE FOR THE FOLLOWING GOLD MINING STOCKS:"
     # puts "1. Ticker: NEM - Name: Newmont Mining"
     # puts "2. Ticker: GG - Name: Goldcorp"
-    
+
     # calls on .make_quotes and then iterates over all of the quotes that gets created to print a list of quotes
-    @quotes = GoldMiners::Scraper.new.make_quotes #creates an instance of Scraper class to store in instance variable @quotes
+    @quotes = GoldMiners::Scraper.new.make_quotes #creates an instance of Scraper class to store array of quotes in instance variable @quotes
     @quotes.each.with_index(1) do |quote, i|
       puts "#{i}. Ticker: #{quote.ticker} Name: #{quote.name}"
      end
